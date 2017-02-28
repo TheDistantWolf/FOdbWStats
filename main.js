f0d47c2f413d40a8f6bd5cd37c17a4389273e9e2
TheDistantWolf <thatdistantwolf@gmail.com>
2017-02-28 09:50:24 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+var title = document.querySelector("#greeting");
+title.innerText = "Fantasy Online";
+
+
+// Random Item
+var item = items[Math.floor(Math.random()*items.length)];
+
+
+// F8F8F8 is white
+// FFFFFF is black
